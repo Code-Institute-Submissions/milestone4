2816e64e59a49ca7abc7a32d509c055ad3af6df0
Phil Surgenor <philsurgenor87@gmail.com>
2018-11-04 19:52:12 +0000

    Initial Commit - Routes Added

---
+++ cookbook/app.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+  
+@app.route('/recipes')
+def recipes():
+  return render_template('recipes/index.html')
+  
+@app.route('/recipes/entry')
+def view_recipe():
+  return render_template('recipes/entry.html')
+  
+@app.route('/add-recipe')
+def add_recipe():
+  return render_template('recipes/add.html')
+  
+@app.route('/edit')
+def edit_recipe():
+  return render_template('recipes/edit.html')
+  
+  
+@app.route('/stats')
+def view_stats():
+  return render_template('stats.html')
+
+    
+    
+if __name__ == '__main__':
+  app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
+    debug=True)
