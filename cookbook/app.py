782a2de37570854798a54ac7b827717777b2497f
Phil Surgenor <philsurgenor87@gmail.com>
2018-11-23 09:57:14 +0000

    Logic added to pull recipe titles from database, check them against user input to see if they already exist and show a warning if true

--- cookbook/app.py
+++ cookbook/app.py
@@ -1,8 +1,9 @@
 import os
 import db_config
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
@@ -10,6 +11,14 @@ app.config["MONGO_DBNAME"] = db_config.database_name
 app.config["MONGO_URI"] = db_config.database_uri
 
 mongo = PyMongo(app)
+
+
+# HELPER FUNCTIONS
+
+#Get Recipe Titles
+def get_recipe_titles():
+  titles = dumps(mongo.db.recipes.find({}, {"_id": 0, "recipe_title": 1}))
+  return titles
 
 
 # The homepage will render home.html template
@@ -31,9 +40,44 @@ def view_recipe(recipe_id):
   recipe_entry = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
   return render_template('recipes/entry.html', recipe=recipe_entry)
   
+
+# To Insert the recipe there are a few things to do before the data can be added to the collection
+# Because of this I can't just use "recipes.insert_one(request.form.to_dict())"
+
+# The recipe title must be unique, this will stop confusion and help with the search.
+# It also means there is something to tell two recipes apart even if they are for the same dish.
 @app.route('/add-recipe')
 def add_recipe():
-  return render_template('recipes/add.html')
+  titles = get_recipe_titles()
+  return render_template('recipes/add.html', titles=titles)
+
+# For the prep time and cook time, I couldn't just have a text input as ther are many ways to add time
+# For example some users might just insert minutes, while others might use hours and minutes
+# So that there is no confusion they will have to insert hours and minutes.
+# I will insert the time as minutes in the collection.
+
+# I have added a field called 'main ingedient'. The will help with search and data visualisation
+# Before Adding it to the collection I will check the main_ingredients collection to see if it is unique
+# If it doesn't already appear in main_ingredients I will add it.
+
+# To save the user time, instead of having to type a list of allergens, there will be  a pre-made list
+# In another collection "allergens", there will be a list of the most common. These appear as checkboxes
+# As allergic reactions can be very serious, I have inserted a text field for other allergens
+# This will cover any I have missed.
+
+# Adding the vegan friendly switch, means I can display clearly if the recipe is suitable for vegans.
+# This will help with the overall User experience.
+@app.route('/insert-recipe', methods=["POST"])
+def insert_recipe():
+  # form_results = request.form.to_dict()
+  # form_results["recipe_title"] = "new title"
+  # element = form_results
+  
+  # recipes = mongo.db.recipes
+  # this_id = recipes.insert_one(request.form.to_dict())
+  # recipe_id = this_id.inserted_id
+  return render_template('test.html')
+  # return redirect(url_for('view_recipe', recipe_id=recipe_id ))
   
 @app.route('/edit')
 def edit_recipe():
